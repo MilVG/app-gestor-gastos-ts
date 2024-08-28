2eaf91ce24988ec609a79abc223ed9d1449c32ae
notlim_dev <elmotigre6@gmail.com>
2024-08-27 22:47:53 -0500

    003: Importación e Implementación- Formulario Presupuesto
    Creación de Formulario para definir el presupuesto de la gestion de
    gastos.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import PresupuestoForm from "./components/PresupuestoForm"
 
 function App() {
   return (
     <>
-      <h1 className="text-blue-700 font-bold uppercase text-3xl">App gestor de gastos</h1>
+      <header className="bg-blue-700">
+        <h1 className="text-white font-bold uppercase text-3xl text-center p-3">App gestor de gastos</h1>
+      </header>
+
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        <PresupuestoForm />
+      </div>
     </>
   )
 }
---
+++ src/components/PresupuestoForm.tsx
@@ -0,0 +1,41 @@
+import { ChangeEvent, useState } from "react"
+
+
+export default function PresupuestoForm() {
+
+  const [presupuesto, setPresupuesto] = useState(0)
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+
+    console.log(e.target.name);
+    console.log(e.target.id);
+
+
+  }
+  return (
+    <form className="bg-red-300 space-y-5 py-2 px-2">
+      <div className=" flex flex-col space-y-5">
+        <label htmlFor="presupuesto-input" className="text-4xl text-blue-600 font-bold text-center">
+          Definir presupuesto
+        </label>
+        <input
+          type="number"
+          id="presupuesto-input"
+          placeholder="S/.300"
+          name="presupuesto-input"
+          className="w-full bg-white border-gray-200 p-2"
+          value={presupuesto}
+          onChange={handleChange}
+        />
+      </div>
+
+      <input
+        type="submit"
+        value='Inicar Presupuesto'
+        className=" bg-blue-600 text-white font-bold rounded-lg w-full p-3 uppercase  cursor-pointer hover:bg-blue-700"
+
+      />
+    </form>
+  )
+}
+
