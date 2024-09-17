6ac2383637fd2ce4563b2f7ec2d69e3840b436bb
notlim_dev <elmotigre6@gmail.com>
2024-09-16 21:01:18 -0500

    014: incorporación de Provider Context
    Se envolvió el provider de manera general para pasar los parametros que
    contiene el context.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { PresupuestoProvider } from './context/PresupuestoContext.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PresupuestoProvider>
+      <App />
+    </PresupuestoProvider>
   </StrictMode>,
 )
